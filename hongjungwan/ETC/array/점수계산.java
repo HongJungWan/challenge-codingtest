87c731c4f1d831c6756cc90a791b2aae08454430
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-11-20 20:09:52 +0900

    [ETC] 점수계산 / 사설 / 3분

---
+++ hongjungwan/ETC/array/점수계산.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Main {
+
+    public int solution(int n, int[] arr) {
+        int answer = 0, cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 1) {
+                cnt++;
+                answer += cnt;
+            } else cnt = 0;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+
+        System.out.println(T.solution(n, arr));
+    }
+
+}
