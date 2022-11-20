4a31f7772d7e7001f52c81fc5d8d950eb8a7895f
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-11-20 15:36:47 +0900

    [ETC] 가위바위보 / 사설 / 5분

---
+++ hongjungwan/ETC/array/가위바위보.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class Main {
+    public String solution(int n, int[] a, int[] b) {
+        String answer = "";
+
+        for (int i = 0; i < n; i++) {
+            if (a[i] == b[i]) answer += "D";
+            else if (a[i] == 1 && b[i] == 3) answer += "A";
+            else if (a[i] == 2 && b[i] == 1) answer += "A";
+            else if (a[i] == 3 && b[i] == 2) answer += "A";
+            else answer += "B";
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int[] a = new int[n];
+        int[] b = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = kb.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            b[i] = kb.nextInt();
+        }
+
+        for (char x : T.solution(n, a, b).toCharArray()) {
+            System.out.println(x);
+        }
+    }
+
+}
