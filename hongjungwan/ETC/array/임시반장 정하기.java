e5929e71772576d9900251907f079dd9d3b1e115
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-11-24 19:26:54 +0900

    [ETC] 임시반장 정하기 / 사설 / 30분

---
+++ hongjungwan/ETC/array/임시반장 정하기.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class Main {
+    public int solution(int n, int[][] arr) {
+        int answer = 0, max = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int cnt = 0;
+            for (int j = 1; j <= n; j++) {
+                for (int k = 1; k <= 5; k++) {
+                    if (arr[i][k] == arr[j][k]) {
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+            if (cnt > max) {
+                max = cnt;
+                answer = i;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int[][] arr = new int[n + 1][6];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 5; j++) {
+                arr[i][j] = kb.nextInt();
+            }
+        }
+
+        System.out.print(T.solution(n, arr));
+    }
+
+}
