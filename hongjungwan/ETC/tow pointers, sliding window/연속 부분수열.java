27733663f4b9bff54f085a38054a335be8763997
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-11-27 05:08:25 +0900

    [ETC] 연속 부분 수열 / 사설 / 10분

---
+++ hongjungwan/ETC/tow pointers, sliding window/연속 부분수열.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class Main {
+    public int solution(int n, int m, int[] arr) {
+        int answer = 0, sum = 0, lt = 0;
+
+        for (int rt = 0; rt < n; rt++) {
+            sum += arr[rt];
+            if (sum == m) answer++;
+            while (sum >= m) {
+                sum -= arr[lt++];
+                if (sum == m) answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+
+        System.out.print(T.solution(n, m, arr));
+    }
+
+}
