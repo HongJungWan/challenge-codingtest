b5184460e5949be1c9cf11ba2357c77391e80244
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-11-28 19:45:55 +0900

    [ETC] 학급 회장 / 사설 / 10분

---
+++ hongjungwan/ETC/hashmap, treeset/학급 회장.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+class Main {
+    public char solution(int n, String s) {
+        char answer = ' ';
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char x : s.toCharArray()) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (char key : map.keySet()) {
+            if (map.get(key) > max) {
+                max = map.get(key);
+                answer = key;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        String str = kb.next();
+
+        System.out.println(T.solution(n, str));
+    }
+
+}
