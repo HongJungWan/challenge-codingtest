041fd9d2d5997a1c3b8376dbdd41e75c35a1ae73
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-11-28 18:45:29 +0900

    [ETC] 최대 길이 연속부분 수열 / 사설 / 20분

---
+++ hongjungwan/ETC/tow pointers, sliding window/최대 길이 연속부분 수열.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class Main {
+    public int solution(int n, int k, int[] arr) {
+        int answer = 0, cnt = 0, lt = 0;
+
+        for (int rt = 0; rt < n; rt++) {
+            if (arr[rt] == 0) cnt++;
+            while (cnt > k) {
+                if (arr[lt] == 0) cnt--;
+                lt++;
+            }
+            answer = Math.max(answer, rt - lt + 1);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+
+        System.out.println(T.solution(n, k, arr));
+    }
+
+}
