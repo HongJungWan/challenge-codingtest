cb7cc5a62cc8bb99cf5ab4d01de1c6b4b3376e67
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-11-29 09:05:41 +0900

    [ETC] 매출액의 종류 / 사설 / 20분

---
+++ hongjungwan/ETC/hashmap, treeset/매출액의 종류.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+class Main {
+    public ArrayList<Integer> solution(int n, int k, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        HashMap<Integer, Integer> HM = new HashMap<>();
+
+        for (int i = 0; i < k - 1; i++) {
+            HM.put(arr[i], HM.getOrDefault(arr[i], 0) + 1);
+        }
+
+        int lt = 0;
+        for (int rt = k - 1; rt < n; rt++) {
+            HM.put(arr[rt], HM.getOrDefault(arr[rt], 0) + 1);
+            answer.add(HM.size());
+
+            HM.put(arr[lt], HM.get(arr[lt]) - 1);
+            if (HM.get(arr[lt]) == 0) HM.remove(arr[lt]);
+            lt++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+
+        for (int x : T.solution(n, k, arr)) System.out.print(x + " ");
+    }
+
+}
