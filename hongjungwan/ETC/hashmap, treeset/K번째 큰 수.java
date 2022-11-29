eab33215ab7cbc9defbb8f75b5a79ac16d8bbc6d
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-11-29 17:26:55 +0900

    [ETC] K 번째 큰 수 / 사설 / 10분

---
+++ hongjungwan/ETC/hashmap, treeset/K번째 큰 수.java
@@ -0,0 +1,43 @@
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+class Main {
+    public int solution(int[] arr, int n, int k) {
+        int answer = -1;
+
+        TreeSet<Integer> Tset = new TreeSet<>(Collections.reverseOrder());
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int l = j + 1; l < n; l++) {
+                    Tset.add(arr[i] + arr[j] + arr[l]);
+                }
+            }
+        }
+
+        int cnt = 0;
+        for (int x : Tset) {
+            cnt++;
+            if (cnt == k) return x;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+
+        System.out.println(T.solution(arr, n, k));
+    }
+
+}
